f7c62d8899992ed9d5e174b743c76a6366886a2d
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-05 10:48:20 +0700

    commit kedua

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,3 +2,18 @@
 #include <string>
 
 using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
