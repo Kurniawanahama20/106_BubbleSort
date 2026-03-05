39e091c6f5cbde9acd9e8a8a5c00b11a1e3549d2
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-03-05 11:35:30 +0700

    comit kelima "menampilkan Array"

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -50,3 +50,18 @@ void bubbleSortArray() {
 
     } while (pass <= n - 1);
 }
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl; //menampilkan Array
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j];
+        if (j < n - 1) {
+            cout << "-->";
+        }
+    }
+    
+}
